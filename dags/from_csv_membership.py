94a4cab5c7d12931e313b1b157de64b8da2652a4
Charis Chakim <132967245+CharisChakim@users.noreply.github.com>
2024-02-02 23:36:20 +0700

    EL csv

---
+++ dags/from_csv_membership.py
@@ -0,0 +1,47 @@
+from airflow.hooks.postgres_hook import PostgresHook
+import pandas as pd
+
+#  Main function that orchestrates the Extract and Load process, so this scripts can be callabe
+def main():
+    read_data()
+    load_data_to_postgres()
+
+def read_data():
+    file_path = '/opt/airflow/dataset/order_details.csv'
+    df = pd.read_csv(file_path)
+    return df
+
+def load_data_to_postgres():
+    # Get postgres connection from airflow
+    pg_hook = PostgresHook(postgres_conn_id='pg_conn')
+    
+    # Define SQL script to create table in postgres
+    create_table_query = '''
+    CREATE TABLE IF NOT EXISTS membership (
+        membership_id INT PRIMARY KEY,
+        membership_amount INT,
+        currency INT,
+        renewal_cycle INT,
+        membership_plan INT,
+        creation_date INT,
+        email TEXT,
+        company TEXT,
+        billing_address TEXT,
+        key_account_manager TEXT,
+        animation_team TEXT
+    )
+    '''
+    # Execute SQL scripts
+    pg_hook.run(create_table_query)
+
+    data = read_data()
+
+    # Specify the columns to be used as the primary key
+    index_columns = ['membership_id']
+
+    # Use the to_sql method to insert data into PostgreSQL
+    data.to_sql('membership', con=pg_hook.get_sqlalchemy_engine(), index=False, if_exists='replace', method='multi', chunksize=1000)
+
+    #close connection
+    pg_hook.get_conn().commit()
+    pg_hook.get_conn().close()
