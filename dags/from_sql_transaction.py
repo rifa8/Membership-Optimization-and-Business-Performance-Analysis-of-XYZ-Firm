fea5ec5905a5abeb490a6d8dc2f5ece2b8a8c566
rifa8 <rifamuhammad812@gmail.com>
2024-02-04 03:00:12 +0000

    remove repetition

--- dags/from_sql_transaction.py
+++ dags/from_sql_transaction.py
@@ -10,8 +10,8 @@ def read_data():
     file_path = 'dataset/membership_transactions.sql'
     with open(file_path, 'r', encoding='utf-8-sig') as file:
         data = file.read()
-        queries = data.replace('`', '')
-    return queries
+        query = data.replace('`', '')
+    return query
 
 def load_data_to_postgres():
     # Get postgres connection from airflow
@@ -35,12 +35,10 @@ def load_data_to_postgres():
     '''
     # Execute SQL scripts
     pg_hook.run(create_table_query)
-    pg_hook.get_conn().commit()
 
-    # Insert data with one query
     insert_data_query = read_data() 
     pg_hook.run(insert_data_query)
-    pg_hook.get_conn().commit()
-    
+
     #close connection
-    pg_hook.get_conn().close()
+    pg_hook.get_conn().commit()
+    pg_hook.get_conn().close()    
