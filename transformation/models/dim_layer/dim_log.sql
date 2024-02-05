c1b12f16ebb679e2492ad6d16071c172c3e379e8
rifa8 <rifamuhammad812@gmail.com>
2024-02-05 09:05:12 +0000

    add dim sql

---
+++ transformation/models/dim_layer/dim_log.sql
@@ -0,0 +1,13 @@
+SELECT
+    membership_id,
+    event_name,
+    upsell_date,
+    new_renewal_cycle,
+    membership_amount,
+    currency,
+    renews_at,
+    new_plan,
+    churn_date,
+    cancellation_date,
+    log_creation_time
+FROM membership_log
---
+++ transformation/models/dim_layer/dim_membership.sql
@@ -0,0 +1,13 @@
+SELECT
+    membership_id,
+    membership_amount,
+    currency,
+    renewal_cycle,
+    membership_plan,
+    creation_date,
+    email,
+    company,
+    billing_address,
+    key_account_manager,
+    animation_team
+FROM membership
