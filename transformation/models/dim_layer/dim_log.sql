f6e7acdb9a4abae27a2d0e791045e9c4e3b355d9
rifa8 <rifamuhammad812@gmail.com>
2024-02-05 09:12:45 +0000

    add currency

--- transformation/models/dim_layer/dim_log.sql
+++ transformation/models/dim_layer/dim_log.sql
@@ -4,7 +4,12 @@ SELECT
     upsell_date,
     new_renewal_cycle,
     membership_amount,
-    currency,
+    CASE
+        WHEN currency = 'EUR' THEN 'USD'
+        WHEN currency = 'GBP' THEN 'USD'
+        WHEN currency = 'DKK' THEN 'USD'
+        ELSE currency
+    END AS currency,
     renews_at,
     new_plan,
     churn_date,
