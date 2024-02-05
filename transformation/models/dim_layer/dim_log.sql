3dc848fffb83883fb0e0f3e3ab9bed50cdbcc03f
rifa8 <rifamuhammad812@gmail.com>
2024-02-05 09:26:03 +0000

    update dim_log

--- transformation/models/dim_layer/dim_log.sql
+++ transformation/models/dim_layer/dim_log.sql
@@ -3,7 +3,11 @@ SELECT
     event_name,
     upsell_date,
     new_renewal_cycle,
-    membership_amount,
+    CASE
+        WHEN currency ='EUR' THEN membership_amount*1.1
+        WHEN currency ='GBP' THEN membership_amount*1.25
+        WHEN currency ='DKK' THEN membership_amount*0.14
+        ELSE membership_amount,
     CASE
         WHEN currency = 'EUR' THEN 'USD'
         WHEN currency = 'GBP' THEN 'USD'
