179bc44111761e8f12ebfc13b566d8e575aaa5c0
Charis Chakim <132967245+CharisChakim@users.noreply.github.com>
2024-02-05 13:35:10 +0700

    add cleaning data membership

--- README.md
+++ README.md
@@ -12,9 +12,9 @@ PT. XYZ menyimpan banyak informasi tentang bisnis afiliasi ini dan status keangg
 
 Tingkat dan manajemen C tertarik untuk mengetahui:
 
-1. Memastikan data pertumbuhan dan historis kepesertaan PT. XYZ selama jangka waktu yang disediakan.
+1. Memastikan data pertumbuhan dan historis kepesertaan PT. XYZ selama jangka waktu yang disediakan. 
 2. Hitung tingkat churn.
-3. Mengkonversi dan mengkonsolidasikan seluruh transaksi pembayaran menjadi perhitungan pendapatan dalam satu mata uang (USD).
+3. Mengkonversi dan mengkonsolidasikan seluruh transaksi pembayaran menjadi perhitungan pendapatan dalam satu mata uang (USD). 
 4. Distribusi keanggotaan, berdasarkan negara, pengelola akun, dan rencana keanggotaan.
 
 Untuk tingkat C, harap tambahkan setidaknya satu atau lebih wawasan yang Anda yakini memiliki nilai bisnis dengan menggunakan analisis data eksplorasi (EDA). Log harus digunakan sebagai dasar untuk melacak pembaruan keanggotaan, churn, dan pembatalan. Pendapatan keanggotaan harus dihitung berdasarkan transaksi; namun, perlu diketahui bahwa mungkin ada perbedaan harga dan mata uang khusus. Selain itu, jika Anda mengalami masalah atau masalah apa pun selama proses tersebut, harap komunikasikan dengan jelas dan terbuka.
---
+++ cleaning_data_membership.sql
@@ -0,0 +1,62 @@
+SELECT * FROM public.membership;
+
+-- Menampilkan jumlah nilai yang hilang untuk setiap kolom
+SELECT 
+    COUNT(*) AS total_rows,
+    COUNT(membership_id) AS missing_id,
+    COUNT(membership_amount) AS missing_amount,
+    COUNT(currency) AS missing_curr,
+    COUNT(renewal_cycle) AS missing_renew,
+    COUNT(membership_plan) AS missing_plan,
+    COUNT(creation_date) AS missing_createdate,
+    COUNT(email) AS missing_mail,
+    COUNT(company) AS missing_company,
+    COUNT(billing_address) AS missing_bill,
+    COUNT(key_account_manager) AS missing_key_manage,
+    COUNT(animation_team) AS missing_anime
+FROM membership;
+
+-- cek kolom membership untuk memastikan
+select membership_id  from membership m where membership_id  isnull 
+
+-- hapus null value
+delete from membership where membership_id isnull 
+
+-- Menampilkan duplikat berdasarkan semua kolom
+select * from  membership m 
+group by 
+   membership_id, membership_amount, currency, renewal_cycle, membership_plan, creation_date, email, company, billing_address, key_account_manager, animation_team
+  HAVING COUNT(*) > 1
+-- aman tidak ada duplikasi data
+
+-- Cek nilai unik di tabel membership_amount
+ select distinct(membership_amount) from membership m  
+ 
+-- Cek nilai unik di tabel currency
+ select distinct(currency) from membership m  
+ 
+-- Cek nilai unik di tabel renewal_cycle
+ select distinct(renewal_cycle) from membership m  
+ 
+ -- Cek nilai unik di tabel membership_plan
+ select distinct(membership_plan) from membership m 
+ 
+ -- Cek nilai unik di tabel creation_date
+ select distinct(creation_date) from membership m;
+--ubah tipe data creation_date
+ALTER TABLE public.membership ALTER COLUMN creation_date TYPE date USING creation_date::date;
+
+ -- Cek nilai unik di tabel email
+ select distinct(email) from membership m;
+
+ -- Cek nilai unik di tabel company
+ select distinct(company) from membership m;
+
+ -- Cek nilai unik di tabel billing_address
+ select distinct(billing_address) from membership m;
+
+ -- Cek nilai unik di tabel key_account_manager
+ select distinct(key_account_manager) from membership m;
+
+-- Cek nilai unik di tabel animation_team
+ select distinct(animation_team) from membership m;
